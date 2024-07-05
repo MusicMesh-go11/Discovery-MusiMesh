2a6fd0bc7c0ff885306f5608f4745c5c4a48e9ec
fazliddin <hajrullaevfazliddin058@gmail.com>
2024-07-05 10:21:40 +0500

    crud for user interaction full

--- storage/uInteraction/methods.go
+++ storage/uInteraction/methods.go
@@ -1,1 +1,22 @@
 package uInteraction
+
+import (
+	c "MusicMesh/Discovery-MusicMesh/generate/composition"
+	u "MusicMesh/Discovery-MusicMesh/generate/user"
+	pb "MusicMesh/Discovery-MusicMesh/generate/user_interactions"
+	"database/sql"
+	"google.golang.org/grpc"
+)
+
+type UserInteraction struct {
+	pb.UnimplementedUserInteractionsServiceServer
+	User        u.UserServiceClient
+	Composition c.CompositionServiceClient
+	DB          *sql.DB
+}
+
+func NewUserInteraction(db *sql.DB, UserConn *grpc.ClientConn, CompConn *grpc.ClientConn) *UserInteraction {
+	user := u.NewUserServiceClient(UserConn)
+	composition := c.NewCompositionServiceClient(CompConn)
+	return &UserInteraction{User: user, Composition: composition, DB: db}
+}
